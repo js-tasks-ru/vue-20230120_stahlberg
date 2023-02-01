5e832152a4c4c52605d496b2a0a1107ce709c5cb
Stahlberg <stahlberg@mail.ru>
2023-02-02 02:36:53 +0300

    Solve 02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,34 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const calculatorComponent = {
+  name: 'calculatorComponent',
+
+  data() {
+    return {
+      number1: '',
+      number2: '',
+      operation: 'sum',
+    };
+  },
+
+  computed: {
+    result() {
+      if (!Number.isFinite(this.number1)) return '';
+      if (!Number.isFinite(this.number2)) return '';
+
+      const resultCalculator = {
+        sum: (num1, num2) => num1 + num2,
+        subtract: (num1, num2) => num1 - num2,
+        multiply: (num1, num2) => num1 * num2,
+        divide: (num1, num2) => num1 / num2,
+      };
+
+      const resultValue = resultCalculator[this.operation](this.number1, this.number2);
+      return Number.isFinite(resultValue) ? resultValue : '';
+    },
+  },
+};
+
+const calculatorApp = createApp(calculatorComponent);
+
+const vm = calculatorApp.mount('#calculator');
